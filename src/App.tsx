cff76b325c37506df5d4b22586fc0d99da2064a2
Ikenna Okonkwo <kennasoft@Ikennas-MacBook-Pro.local>
2020-12-03 11:18:29 -0500

    main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+// import './App.css';
+import ReportView from './components/ReportView';
+import { ReceiptsContext } from './contexts/receipts-context';
+import { ReportContext } from './contexts/report-context';
 
 function App() {
+  const { report, exchangeRates } = useContext(ReportContext);
+  const { receipts, setReceipts, updateReceipts, deleteReceipt } = useContext(ReceiptsContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReportView
+      company={report.company}
+      receipts={receipts}
+      setReceipts={setReceipts}
+      updateReceipts={updateReceipts}
+      deleteReceipt={deleteReceipt}
+      exchangeRates={exchangeRates}
+    />
   );
 }
 
